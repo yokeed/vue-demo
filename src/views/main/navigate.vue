0cfa919806cf66691c8574e5623922127dff9f9a
yong <yokeed@163.com>
2019-10-12 11:16:47 +0800

    vue demo

---
+++ src/views/main/navigate.vue
@@ -0,0 +1,17 @@
+<template>
+<el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</el-container>
+</template>
+<script>
+export default {
+  name: 'navigate'
+}
+</script>
+<style>
+</style>
